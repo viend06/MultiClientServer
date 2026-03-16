d5ec011ced19c4ac5ccd4da9297bae866c0a0d2e
viend06 <vienduy06@gmail.com>
2026-03-16 09:21:55 +0700

    refactor: move readFromFile, saveFile, and check functions into separate file

--- Server.cpp
+++ Server.cpp
@@ -216,45 +216,6 @@ public:
         }
     }
 
-    bool check(const string &name, const string &password)
-    {
-        lock_guard<mutex> lock(mtx);
-
-        auto it = list_user.find(name);
-        if (it == list_user.end())
-            return false;
-        return it->second == password;
-    }
-
-    void saveToFile(const string &name, const string &pw)
-    {
-        {
-            lock_guard<mutex> lock(mtx);
-            ofstream file("user.txt", ios::app);
-            file << name << " " << pw << endl;
-            file.close();
-        }
-    }
-
-    void readFromFile()
-    {
-        {
-            lock_guard<mutex> lock(mtx);
-            ifstream file("user.txt");
-            string information;
-            while (getline(file, information))
-            {
-                stringstream ss(information);
-                string name, password;
-                ss >> name >> password;
-                list_user[name] = password;
-            }
-            file.close();
-            // ofstream clear("user.txt", ios::trunc);
-            // clear.close();
-        }
-    }
-
     // Deconstructor
     ~Socket()
     {
