488e6496de2d20cb9ea4e2f586c4af94deb675a1
viend06 <vienduy06@gmail.com>
2026-03-21 23:14:18 +0700

    refactor(file): remove mutex lock from saveToFile

--- FileIO/FileHandler.cpp
+++ FileIO/FileHandler.cpp
@@ -2,21 +2,21 @@
 #include "login/Login.h"
 using namespace std;
 
-void saveToFile(const string &name, const string &pw){
-    {
-        lock_guard<mutex> lock(mtx);
-        ofstream file("user.txt", ios::app);
-        file << name << " " << pw << endl;
-        file.close();
-    }
+void saveToFile(const string &name, const string &pw)
+{
+    ofstream file("user.txt", ios::app);
+    file << name << " " << pw << endl;
+    file.close();
 }
 
-void readFromFile(){
+void readFromFile()
+{
     {
         lock_guard<mutex> lock(mtx);
         ifstream file("user.txt");
         string information;
-        while(getline(file, information)){
+        while (getline(file, information))
+        {
             stringstream ss(information);
             string name, password;
             ss >> name >> password;
